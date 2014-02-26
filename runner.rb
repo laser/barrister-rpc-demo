a30f4f176eff92c9b593391dc64b82b6530dc11d
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-02-25 17:04:55 -0800

    Bumping barrister-intraprocess version to 0.2.0

--- runner.rb
+++ runner.rb
@@ -3,8 +3,8 @@ require 'barrister-intraprocess'
 require_relative './client/session'
 require_relative './service/user_service'
 
-container  = Barrister::Containers::IntraProcess.new './service/user_service.json', UserService
-transport  = Barrister::Transports::IntraProcess.new container
+container  = Barrister::IntraProcessContainer.new './service/user_service.json', [UserService]
+transport  = Barrister::IntraProcessTransport.new container
 rpc_client = Barrister::Client.new transport
 session    = TerminalClient::Session.new rpc_client
 session.begin
